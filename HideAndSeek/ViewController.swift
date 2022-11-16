7e8d060acfcb67b4f2576b50104dc3a28b9e21b9
Nastyfrisky <stpnkvnst@mail.ru>
2022-11-16 17:31:59 +0300

    Initial Commit

---
+++ HideAndSeek/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HideAndSeek
+//
+//  Created by Анастасия Ступникова on 16.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
